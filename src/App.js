138e9d5d39f3fb1af500202662f0353221099d8a
Dadash Hussein <huseynzade.dadas@gmail.com>
2023-01-06 10:06:29 +0400

    start-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Weather from "./components/Weather";
 
 function App() {
+  const [inpCity, setInpCity] = useState("");
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ccce7999e11cd17fa951c988128c7c0c&units=metric`
+      );
+      setWeather(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [city]);
+
+  const handleChange = (e) => {
+    setInpCity(e.target.value);
+  };
+
+  const eventSubmit = (e) => {
+    e.preventDefault();
+    setCity(inpCity);
+    setInpCity("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Weather data={weather} />
+      <input className="input" type="text" onChange={handleChange} />
+      <button className="search__b" onClick={eventSubmit}>
+        Search
+      </button> <i class="uil uil-airplay"></i>
     </div>
   );
 }
